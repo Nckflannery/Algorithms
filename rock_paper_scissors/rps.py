509e3a95d3e3c6ea430395584a93d25dae27b57f
Nckflannery <Nckflannery@users.noreply.github.com>
2020-03-18 15:34:07 -0700

    RPS complete

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,25 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  choices = [['rock'], ['paper'], ['scissors']]
+  output = []
+  # Base cases
+  if n <= 0:
+    return [[]]
+  if n == 1:
+    return choices
+  looper = rock_paper_scissors(n-1)
+  # Eventually rock_paper_scissors(n-1) will return choices, so:
+  for i in looper:
+    # iterate through choices
+    for j in choices:
+      # combine them
+      k = i + j
+      # append to output
+      output.append(k)
+  
+  return output
+    
 
 
 if __name__ == "__main__":
